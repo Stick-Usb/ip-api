6fcef6f23ec4f0b0a13f2232c23797a543808676
Stick-Usb <155399743+Stick-Usb@users.noreply.github.com>
2024-01-03 14:39:03 +0100

    Delete index.js

--- index.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const app = express();
-
-app.get('/',function(req, res) {
-    const ipAddress = req.socket.remoteAddress;
-    res.send(ipAddress);
-});
-
-app.listen(3000, () => console.log(`Server is listening on port 3000`))
