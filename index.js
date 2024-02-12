2570909d6450f1a7f0684b84f6fa3337ff06302a
Stick-Usb <155399743+Stick-Usb@users.noreply.github.com>
2024-02-12 19:40:09 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -1,46 +1,15 @@
-// Import the required modules
-const os = require('os');
-const net = require('net');
-const http = require('http');
+const express = require('express');
+const app = express();
 
-// Define a function to get the public IP address
-function getPublicIP() {
-  // Get all the network interfaces
-  let interfaces = os.networkInterfaces();
-  // Loop through each interface
-  for (let name in interfaces) {
-    // Loop through each address of the interface
-    for (let address of interfaces[name]) {
-      // Check if the address is not internal and is IPv4
-      if (!address.internal && net.isIPv4(address.address)) {
-        // Return the address
-        return address.address;
-      }
-    }
-  }
-  // Return null if no public IP address is found
-  return null;
-}
-
-// Create a web server
-const server = http.createServer((req, res) => {
-  // Get the public IP address
-  let ip = getPublicIP();
-  // Check if the IP address is valid
-  if (ip) {
-    // Set the response headers
-    res.setHeader('Content-Type', 'application/json');
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    // Send the JSON object with the IP address
-    res.end(JSON.stringify({ip: ip}));
-  } else {
-    // Send an error message
-    res.statusCode = 500;
-    res.end('Could not get the public IP address');
-  }
+app.get('/', (req, res) => {
+  const clientIP = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+  console.log(`Client IP address: ${clientIP}`);
+  const ipAddress = req.ip;
+  console.log(`Client IP address: ${ipAddress}`);
+  
+  res.send(clientIP+','+ipAddress);
 });
 
-// Listen on port 3000
-server.listen(3000, () => {
-  console.log('Server running on port 3000');
+app.listen(3000, () => {
+  console.log('Example app is listening on port 3000.');
 });
